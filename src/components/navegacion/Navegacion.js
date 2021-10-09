62d762d688481cf450efd758d8dcc0d8b27e7092
TobiasLongstaff <tobilongstaff@outlook.com>
2021-10-09 00:06:18 -0300

    v1.0.0

---
+++ src/components/navegacion/Navegacion.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Navegacion.css'
+
+const Navegacion = () =>
+(
+    <nav>
+        <h1>Tobias Longstaff</h1>
+        <div className="container-btn">
+            <button className="btn-nav-secundarios">Projectos</button>
+            <button className="btn-nav-secundarios">Sobre mi</button>
+            <button className="btn-nav-principal">Contactame</button>
+        </div>
+    </nav>
+)
+
+export default Navegacion
